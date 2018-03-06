70449d2f4705943ff2da325b3d2e7f8c4e04448f
Michael Neal <michaelneal@Macbook-Pro-15.local>
2018-03-05 22:14:34 -0600

    style components

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import moment from 'moment';
+import styles from './assets/stylesheets/app.css';
 
 var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(rightNow);
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,32 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    // __dirname = location of this config file
     context: path.resolve(__dirname, 'src'),
     entry: {
         app: './app.js'
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    }
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist', 'assets'),
+        publicPath: '/assets'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'src')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/i,
+                exclude: [/node_modules],
+                use: [{
+                    loader: 'babel-loader',
+                    options: { presets: ['env'] },
+                }],
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 };
