7e9162c4e1db90a5e825528ce3b44b5d9516986b
Michael Neal <michaelneal@Macbook-Pro-15.local>
2018-03-05 22:32:05 -0600

    css modules

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,37 @@ const webpack = require('webpack');
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     entry: {
-        app: './app.js'
+        app: './app.js',
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist', 'assets'),
-        publicPath: '/assets'
+        publicPath: '/assets',
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'src')
+        contentBase: path.resolve(__dirname, 'src'),
     },
     module: {
         rules: [
             {
                 test: /\.js$/i,
-                exclude: [/node_modules],
-                use: [{
-                    loader: 'babel-loader',
-                    options: { presets: ['env'] },
-                }],
+                exclude: [/node_modules/],
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: { presets: ['env'] },
+                    },
+                ],
             },
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: { modules: true },
+                    },
+                ],
             },
         ],
     },
